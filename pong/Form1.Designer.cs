c00938b9bb077bf83bc2e101adb6570902f95497
Eric Buchanan <42704734+EricMBu@users.noreply.github.com>
2022-03-26 16:23:33 -0400

    Add files via upload

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.paddle1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // paddle1
+            // 
+            this.paddle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.paddle1.Location = new System.Drawing.Point(12, 196);
+            this.paddle1.Name = "paddle1";
+            this.paddle1.Size = new System.Drawing.Size(16, 63);
+            this.paddle1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 17;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ball.Location = new System.Drawing.Point(384, 196);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(13, 14);
+            this.ball.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.paddle1);
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel paddle1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel ball;
+    }
+}
+
---
+++ pong/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        private Boolean goingUp = false;
+        private Boolean goingDown = false;  
+
+        private Point paddlePoint = new Point(12, 196);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goingUp)
+            {
+                paddlePoint.Y -= 10;
+            }
+            if (goingDown)
+            {
+                paddlePoint.Y += 10;
+            }
+            paddle1.Location = paddlePoint;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down)
+            {
+                goingUp = false;
+                goingDown = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goingUp = true;
+                goingDown = false;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down)
+            {
+                goingDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goingUp = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
