4da4935ff3acfe3e436204465a2c07742eae550c
DymondDust <ericmichael.buchanan@gmail.com>
2022-11-16 16:15:27 -0500

    Added ball movement and collision

--- pong/Form1.Designer.cs
+++ pong/Form1.Designer.cs
@@ -32,11 +32,14 @@
             this.paddle1 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.ball = new System.Windows.Forms.Panel();
+            this.lblScore1 = new System.Windows.Forms.Label();
+            this.lblScore2 = new System.Windows.Forms.Label();
+            this.paddle2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // paddle1
             // 
-            this.paddle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.paddle1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.paddle1.Location = new System.Drawing.Point(12, 196);
             this.paddle1.Name = "paddle1";
             this.paddle1.Size = new System.Drawing.Size(16, 63);
@@ -56,11 +59,45 @@
             this.ball.Size = new System.Drawing.Size(13, 14);
             this.ball.TabIndex = 1;
             // 
+            // lblScore1
+            // 
+            this.lblScore1.AutoSize = true;
+            this.lblScore1.Font = new System.Drawing.Font("MGS2 Menu", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore1.ForeColor = System.Drawing.Color.Crimson;
+            this.lblScore1.Location = new System.Drawing.Point(213, 55);
+            this.lblScore1.Name = "lblScore1";
+            this.lblScore1.Size = new System.Drawing.Size(68, 32);
+            this.lblScore1.TabIndex = 2;
+            this.lblScore1.Text = "0";
+            // 
+            // lblScore2
+            // 
+            this.lblScore2.AutoSize = true;
+            this.lblScore2.Font = new System.Drawing.Font("MGS2 Menu", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore2.ForeColor = System.Drawing.Color.Crimson;
+            this.lblScore2.Location = new System.Drawing.Point(487, 55);
+            this.lblScore2.Name = "lblScore2";
+            this.lblScore2.Size = new System.Drawing.Size(68, 32);
+            this.lblScore2.TabIndex = 3;
+            this.lblScore2.Text = "0";
+            // 
+            // paddle2
+            // 
+            this.paddle2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.paddle2.Location = new System.Drawing.Point(772, 196);
+            this.paddle2.Name = "paddle2";
+            this.paddle2.Size = new System.Drawing.Size(16, 63);
+            this.paddle2.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.paddle2);
+            this.Controls.Add(this.lblScore2);
+            this.Controls.Add(this.lblScore1);
             this.Controls.Add(this.ball);
             this.Controls.Add(this.paddle1);
             this.Name = "Form1";
@@ -69,6 +106,7 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +115,9 @@
         private System.Windows.Forms.Panel paddle1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Panel ball;
+        private System.Windows.Forms.Label lblScore1;
+        private System.Windows.Forms.Label lblScore2;
+        private System.Windows.Forms.Panel paddle2;
     }
 }
 
--- pong/Form1.cs
+++ pong/Form1.cs
@@ -1,21 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace pong
 {
     public partial class Form1 : Form
     {
-        private Boolean goingUp = false;
-        private Boolean goingDown = false;  
+        //scores
+        private int rightScore = 0;
+        private int leftScore = 0;
+        //ball variables
+        private int xDir = 1;
+        private int yDir = 1;
+        private int vel = 6;
+        private Point ballPoint = new Point(384, 196); 
 
-        private Point paddlePoint = new Point(12, 196);
+        //paddle variables
+        private Boolean goingUp1 = false;
+        private Boolean goingDown1 = false;
+        private Boolean goingUp2 = false;
+        private Boolean goingDown2 = false;
+
+        private Point paddlePoint1 = new Point(12, 196);
+        private Point paddlePoint2 = new Point(772, 196);
         public Form1()
         {
             InitializeComponent();
@@ -23,46 +30,118 @@ namespace pong
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (goingUp)
+            //ball movement
+            BallMove();
+            if (ballPoint.Y <= 0 || ballPoint.Y + 3*ball.Height >= this.Height)
             {
-                paddlePoint.Y -= 10;
+                yDir *= -1;
             }
-            if (goingDown)
+
+            bool bounce1 = (ballPoint.X <= paddlePoint1.X + paddle1.Width && ballPoint.Y >= paddlePoint1.Y && ballPoint.Y <= paddlePoint1.Y + paddle1.Height);
+            bool bounce2 = (ballPoint.X + ball.Width >= paddlePoint2.X && ballPoint.Y >= paddlePoint2.Y && ballPoint.Y <= paddlePoint2.Y + paddle2.Height);
+            if (bounce1 || bounce2)
             {
-                paddlePoint.Y += 10;
+                xDir *= -1;
             }
-            paddle1.Location = paddlePoint;
+
+            if (ballPoint.X <= 0)
+            {
+                yDir = 1;
+                leftScore += 1;
+                lblScore1.Text = leftScore.ToString();
+            }
+            if (ballPoint.X + ball.Width >= this.Width)
+            {
+                ball.Location = new Point(384, 196);
+                yDir = -1;
+                rightScore += 1;
+                lblScore2.Text = rightScore.ToString();
+            }
+
+            //paddle1 movement
+            if (goingUp1 && paddlePoint1.Y >= 0)
+            {
+                paddlePoint1.Y -= 10;
+            }
+            if (goingDown1 && (paddlePoint1.Y + 1.5 * paddle1.Height) <= this.Height)
+            {
+                paddlePoint1.Y += 10;
+            }
+
+            //paddle2 movement
+            if (goingUp2 && paddlePoint2.Y >= 0)
+            {
+                paddlePoint2.Y -= 10;
+            }
+            if (goingDown2 && (paddlePoint2.Y + 1.5 * paddle2.Height) <= this.Height)
+            {
+                paddlePoint2.Y += 10;
+            }
+            paddle1.Location = paddlePoint1;
+            paddle2.Location = paddlePoint2;
+        }
+
+        private void BallMove()
+        {
+            ballPoint.X += xDir * vel;
+            ballPoint.Y += yDir * vel;
+            ball.Location = new Point(ballPoint.X, ballPoint.Y);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Down)
+            /*if (e.KeyCode == Keys.Enter)
             {
-                goingUp = false;
-                goingDown = true;
+                rightScore += 1;
+                lblScore1.Text = rightScore.ToString();
+            }*/
+            if (e.KeyCode == Keys.S)
+            {
+                goingUp1 = false;
+                goingDown1 = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                goingUp1 = true;
+                goingDown1 = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goingUp2 = false;
+                goingDown2 = true;
             }
             if (e.KeyCode == Keys.Up)
             {
-                goingUp = true;
-                goingDown = false;
+                goingUp2 = true;
+                goingDown2 = false;
             }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Down)
+            if(e.KeyCode == Keys.S)
             {
-                goingDown = false;
+                goingDown1 = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                goingUp1 = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goingDown2 = false;
             }
             if (e.KeyCode == Keys.Up)
             {
-                goingUp = false;
+                goingUp2 = false;
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            ball.Location = new Point(384, 196);
         }
     }
 }
